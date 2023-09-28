93ed0b5dfcf475b9fc4ebbedaaf7869d473f1a8e
Matheus Sanches <methidezeeni@gmail.com>
2023-09-28 19:49:59 -0300

    chore: allow every header

--- src/main/kotlin/io/musicorum/api/plugins/HTTP.kt
+++ src/main/kotlin/io/musicorum/api/plugins/HTTP.kt
@@ -11,6 +11,7 @@ fun Application.configureHTTP() {
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.Authorization)
+        allowHeader("*")
         anyHost()
     }
 }
