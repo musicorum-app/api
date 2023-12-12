d4d6843cb2899ceea7acb5b162b2bf5d80e3dfd5
MysteryMS <piva.pedro@outlook.com>
2023-12-12 11:28:30 -0300

    import cleanup

--- src/main/kotlin/io/musicorum/api/realms/docs/Routes.kt
+++ src/main/kotlin/io/musicorum/api/realms/docs/Routes.kt
@@ -1,12 +1,8 @@
 package io.musicorum.api.realms.docs
 
 import io.ktor.server.application.*
-import io.ktor.server.plugins.openapi.*
 import io.ktor.server.plugins.swagger.*
 import io.ktor.server.routing.*
-import io.swagger.codegen.languages.StaticHtml2Generator
-import io.swagger.codegen.v3.generators.html.StaticHtml2Codegen
-import io.swagger.codegen.v3.generators.html.StaticHtmlCodegen
 
 fun Application.createDocsRoute() {
     routing {
